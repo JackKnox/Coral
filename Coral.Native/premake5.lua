0d362bd0ef76395d56e41117f7ddf41b3562b2e5
Jack Knox <jackjackfilms1@gmail.com>
2025-03-30 14:31:36 +0100

    Pointed output to default variable

--- Coral.Native/premake5.lua
+++ Coral.Native/premake5.lua
@@ -11,8 +11,8 @@ project "Coral.Native"
     -- Can't specify 64-bit architecture in the workspace level since VS 2022 (see https://github.com/premake/premake-core/issues/1758)
     architecture "x86_64"
 
-	targetdir("../Build/%{cfg.buildcfg}")
-	objdir("../Intermediates/%{cfg.buildcfg}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
     pchheader "CoralPCH.hpp"
     pchsource "Source/CoralPCH.cpp"
